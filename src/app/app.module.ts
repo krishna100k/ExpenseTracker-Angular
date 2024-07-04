e81259af69f401c7cf76749faf4ccfc9c35ad07c
Krishnaprasad Awala <SEZ6436@neilsoft.in>
2024-07-04 18:06:29 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainBoxComponent } from './components/main-box/main-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainBoxComponent,
   ],
   imports: [
     BrowserModule,
