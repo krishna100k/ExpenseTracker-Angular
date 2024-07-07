e8a8e5af195ac4e5c3b7c7328feeb81ad5a407d0
krishna10k <krishnaprasadawala@gmail.com>
2024-07-07 23:11:02 +0530

    changes

--- src/app/components/main-box/main-box.component.ts
+++ src/app/components/main-box/main-box.component.ts
@@ -11,17 +11,19 @@ export class MainBoxComponent {
   INR = "";
 
   addExpense(){
-    console.log("clicked!")
     const newExpense = {
       expense: this.expense,
       INR: this.INR
     }
     if(this.expense!== "" && this.INR!== ""){
-      this.expenses.push(newExpense)
+      this.expenses.push(newExpense);
       alert("Expense Added Successfully!")
     }else{
       alert("Expense Not Found!")
     }
+
+    console.log(this.expenses);
+
   }
 
 }
